43020568d1979a1b545826efe49d210db5307b82
dhwpdnr <ink1237@gmail.com>
2023-09-27 10:43:28 +0900

    numpy test

---
+++ numpy_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+m_3 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [4, 5, 6]
+]
+
+m_4 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [4, 5, 6]
+]
+
+m3_np = np.array(m_3)
+m4_np = np.array(m_4)
+
+m_total = m3_np + m4_np
