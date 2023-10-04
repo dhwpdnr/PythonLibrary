f357e4d7864987d50197ae3d851abff1c303265f
dhwpdnr <ink1237@gmail.com>
2023-10-04 10:34:39 +0900

    [feat] numpy test

--- numpy_test.py
+++ numpy_test.py
@@ -16,3 +16,17 @@ m3_np = np.array(m_3)
 m4_np = np.array(m_4)
 
 m_total = m3_np + m4_np
+
+data = np.arange(1, 5)
+print(data)
+
+data2 = np.linspace(2.0, 3.0)
+print(data2)
+
+data3 = np.eye(2)
+print(data3)
+
+data1 = np.array([10, 20, 30])
+data2 = np.array([1, 2, 3])
+data3 = data1 + data2
+print(data3)
