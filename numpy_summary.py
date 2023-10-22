bcca0a7607a1cf9324a3a5f10d6031fdb22d347c
dhwpdnr <ink1237@gmail.com>
2023-10-22 15:21:57 +0900

    [feat] numpy copy

--- numpy_summary.py
+++ numpy_summary.py
@@ -290,5 +290,44 @@ print(f"np.hsplit(a, 3) : {np.hsplit(a, 3)}")
 
 # [2,12] => [:, :3], [:, 3:4], [:, 4:]로 분할
 print(f"np.hsplit(a, (3, 4)) : {np.hsplit(a, (3, 4))}")
+print()
+
+print("데이터 복사")
+# array를 변수에 넣는다고 해서 복사가 되지 않는다
+a = np.array([[0, 1, 2, 3],
+              [4, 5, 6, 7],
+              [8, 9, 10, 11]])
+# 두 개가 사실상 같습니다. (복사가 아님)
+b = a
+print(b is a)
+print(id(a))
+print(id(b))
+
+print("얕은 복사")
+c = a.view()
+# c와 a의 참조값은 다름
+print(c is a)
+
+c = c.reshape((2, 6))
+print(a.shape)
+# (3, 4)
+
+c[0, 4] = 1234
+print(a)
+
+s = a[:, 1:3]
+s[:] = 10
+print(a)
+print()
+
+print("깊은 복사")
+# 참조값 뿐만 아니라 각각의 데이터 전부가 새로운 객체로 생성
+d = a.copy()
+print(d is a)
+
+# a와 d의 데이터의 참조값도 다름
+d[0, 0] = 9999
+print(a)
+
 
 # np.eye(), np.diag(), np.vander(), np.indices()
