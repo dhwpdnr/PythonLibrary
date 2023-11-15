502bb2a00e296bdddb9eecb647f6c8160de29280
dhwpdnr <ink1237@gmail.com>
2023-11-15 10:00:08 +0900

    [feat] to excel

--- pandas_test/get_file.py
+++ pandas_test/get_file.py
@@ -2,7 +2,13 @@ import pandas as pd
 import numpy as np
 
 file_path = "./data/vehicle_prod.csv"
+file_name = "vehicle.csv"
 
 df = pd.read_csv(file_path)
 
+save_fileName = file_path + "data2.csv"
+df.to_csv(save_fileName, index=False)
+
+save_fileName2 = file_path + "data2.xlsx"
+df.to_excel(save_fileName2, sheet_name="mySheet", index=False)
 print(df)
