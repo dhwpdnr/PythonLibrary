97c61da8373a4230c3fd701bd99a4cb947321a5a
dhwpdnr <ink1237@gmail.com>
2023-11-21 13:36:12 +0900

    [feat] user data set

---
+++ pandas_test/user.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import re
+
+path = "./data/"
+name = "user.csv"
+
+read_fileName = path + name
+df = pd.read_csv(read_fileName)
+user_data = []
+phone_numbers = set()
+phone_regex = re.compile(r'^\d{10,11}$')
+
+for item in df.itertuples():
+    phone_number = str(item[3])
+    if re.match(phone_regex, phone_number) and phone_number not in phone_numbers:
+        user_payload = {
+            "name": item[1],
+            "phone_number": phone_number,
+        }
+        user_data.append(user_payload)
+        phone_numbers.add(phone_number)
+
+print(len(user_data))
