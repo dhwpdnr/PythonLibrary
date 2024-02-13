74e5ba0ea2a7f0eb315474c9e2cd56d4ae535b9c
dhwpdnr <ink1237@gmail.com>
2024-02-13 17:02:27 +0900

    [feat] daejeon temp map

--- forecast/check.py
+++ forecast/check.py
@@ -1,22 +1,24 @@
-import json
+import pandas as pd
+import os
 
-# GeoJSON 파일 로드
-file_path = "./data/HangJeongDong_ver20230701.geojson"
-with open(file_path, "r", encoding="utf-8") as f:
-    geojson_data = json.load(f)
+# 결과 CSV 파일이 저장된 폴더
+output_folder = "./processed_data"
 
-filtered_features = [
-    feature
-    for feature in geojson_data["features"]
-    if feature["properties"]["sido"] == "30"
-]
+# 병합할 DataFrame을 저장할 리스트 초기화
+dfs = []
 
-# 필터링된 데이터로 새로운 GeoJSON 객체 생성
-filtered_geojson = {"type": "FeatureCollection", "features": filtered_features}
+# output_folder 내 모든 CSV 파일을 순회하며 DataFrame으로 로드
+for filename in os.listdir(output_folder):
+    if filename.endswith(".csv"):
+        file_path = os.path.join(output_folder, filename)
+        df = pd.read_csv(file_path)
+        dfs.append(df)
 
-# 결과 확인 (필터링된 항목 수 출력)
-len(filtered_features), filtered_geojson.keys()
+# 모든 DataFrame 병합
+merged_df = pd.concat(dfs, ignore_index=True)
 
-filtered_geojson_file_path = "./data/Daejeon.geojson"
-with open(filtered_geojson_file_path, "w", encoding="utf-8") as f:
-    json.dump(filtered_geojson, f, ensure_ascii=False, indent=4)
+# 병합된 데이터프레임을 새로운 CSV 파일로 저장
+merged_csv_path = os.path.join(output_folder, "merged_avg_tamax_by_region.csv")
+merged_df.to_csv(merged_csv_path, index=False)
+
+print(f"Merged CSV saved to: {merged_csv_path}")
