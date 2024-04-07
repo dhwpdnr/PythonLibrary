9c1d959f6f5d1abc4e76d83641a6b57fdf01c9a8
dhwpdnr <ink1237@gmail.com>
2024-04-07 21:08:50 +0900

    [feat] request

--- bs4/coupang.py
+++ bs4/coupang.py
@@ -1,34 +1,12 @@
 import requests
-import re
-from bs4 import BeautifulSoup
+import json
 
-url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=6&backgroundColor="
+url = "https://m.coupang.com/vm/sdp/v3/mweb/products/6974393264/option-tree-list?itemId=17019560505&vendorItemId=89321486763&expandType=11053&selectedItemId=17019560505&selectedVendorItemId=89321486763&sdp-st=H4sIAAAAAAAAAE2OQQqDMBRE7_LXLowxUbMuhUKhQu0BUv3IhzSWmFiK5O5Voba74c08mBlulnztqMVm6HuDtXb6MYKaIdhXB8q7gDGBy9PTYBt9_18YIlCsSFklZCpSkYCZVlRWPGN5KQvJd_WKBts1bPpi782ZRv-FgB4UZDmvYOm3WyfbmtCR7Q9oaEL3PuLvQowfsvaBacEAAAA&metaData=eyJzIjoiTVdFQl9OT1JNQUxfMCIsImsiOiIyNzNmdnQycWk0a2ZtOWEydzciLCJkIjpbeyJ0IjpbMjQzOV0sImUiOjB9LHsidCI6WzExMDUzXSwiZSI6MH1dLCJvIjoxNCwibCI6eyJwIjo2OTc0MzkzMjY0LCJpIjoxNzAxOTU2MDUwNywidiI6ODkxMTY4NTg4OTJ9LCJlIjp7ImUiOmZhbHNlLCJyIjotMSwibmMiOmZhbHNlLCJzZmIiOnRydWUsInNxYiI6dHJ1ZSwic2JzbmIiOmZhbHNlLCJkYnNuYmlkIjoiQ1JNLTY3MDcwX0xvZ291dF93ZWJfMSIsIm5sYyI6ZmFsc2V9fQ"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
 }
 res = requests.get(url, headers=headers)
-res.raise_for_status()
-
-soup = BeautifulSoup(res.text, "lxml")
-
-items = soup.find_all("li", attrs={"class": re.compile("^search-product")})
-for item in items:
-
-    name = item.find("div", attrs={"class": "name"}).get_text()  # 제품명
-
-    price = item.find("strong", attrs={"class": "price-value"}).get_text()  # 가격
-
-    rate = item.find("em", attrs={"class": "rating"})  # 평점
-    if rate:
-        rate = rate.get_text()
-    else:
-        rate = "평점 없음"
-
-    rate_cnt = item.find("span", attrs={"class": "rating-total-count"})  # 평점 수
-    if rate_cnt:
-        rate_cnt = rate_cnt.get_text()
-    else:
-        rate_cnt = "평점 수 없음"
-
-    print(f"이름 : {name}, 가격 : {price}, 평점 : {rate}, 평점수 : {rate_cnt}")
+if res.status_code == 200:
+    data = json.loads(res.text)
+    print(data["rData"])
