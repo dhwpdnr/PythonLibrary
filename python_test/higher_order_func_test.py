6bb6b8dabf710857c6ba36a129efdf5bb396c000
dhwpdnr <ink1237@gmail.com>
2025-02-09 18:53:47 +0900

    chore: Higer Order Function Example

--- python_test/higher_order_func_test.py
+++ python_test/higher_order_func_test.py
@@ -28,3 +28,38 @@ double = multiplier(2)  # factor=2인 함수 반환
 print(double(5))  # 5 * 2 -> 결과: 10
 triple = multiplier(3)  # factor=3인 함수 반환
 print(triple(5))  # 5 * 3 -> 결과: 15
+
+# 고차함수 사용 사례
+# map
+nums = [1, 2, 3, 4]
+doubled = list(map(lambda x: x * 2, nums))
+print(doubled)  # [2, 4, 6, 8]
+
+# filter
+nums = [1, 2, 3, 4]
+even = list(filter(lambda x: x % 2 == 0, nums))
+print(even)  # [2, 4]
+
+# reduce
+from functools import reduce
+
+nums = [1, 2, 3, 4]
+product = reduce(lambda x, y: x * y, nums)
+print(product)  # 24
+
+
+# 클로저와 사용
+def power(exponent):
+    """제곱을 수행하는 함수 반환"""
+
+    def calculate(base):
+        return base**exponent
+
+    return calculate
+
+
+square = power(2)
+cube = power(3)
+
+print(square(5))  # 5^2 = 25
+print(cube(2))  # 2^3 = 8
